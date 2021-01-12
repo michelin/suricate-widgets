56bc2e4801c3208751517b691cadbe0b473131ea
Loïc Greffier <loic.greffier_ext@michelin.com>
2021-01-12 18:21:07 +0100

    :art: - Beautify jenkins widgets

--- content/jenkins/widgets/jenkinsJobStatus/script.js
+++ content/jenkins/widgets/jenkinsJobStatus/script.js
@@ -1,109 +1,107 @@
 /*
- * Copyright 2012-2018 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+  * Copyright 2012-2018 the original author or authors.
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  *      http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
 
 function run() {
-    var data = {};
-    var jsonResponse = Packages.call(WIDGET_CONFIG_JENKINS_URL + "/job/" + SURI_JOB + "/lastBuild/api/json", null, null, null);
+  var data = {};
+  var jsonResponse = Packages.call(WIDGET_CONFIG_JENKINS_URL + "/job/" + SURI_JOB + "/lastBuild/api/json", null, null, null);
 
-    if (jsonResponse === null) {
-        return null;
-    }
-    
-    var display_mode = "upstream";
-    if (typeof SURI_DISPLAY_MODE !== 'undefined' && SURI_DISPLAY_MODE !== ""){
-        display_mode = SURI_DISPLAY_MODE;
-    }
-    
-    // Convert String to object
-    var jsonObject = JSON.parse(jsonResponse);
-    var user = "";
+  if (jsonResponse === null) {
+    return null;
+  }
 
-    for (var i in jsonObject.actions) {
-        if (jsonObject.actions[i].hasOwnProperty("causes")) {
-            for (var j in jsonObject.actions[i].causes) {
-                if (jsonObject.actions[i].causes[j].hasOwnProperty("shortDescription")) {
-                    if (jsonObject.actions[i].causes[j].hasOwnProperty("userId")) {
-                        data.builtBy = jsonObject.actions[i].causes[j].userId;
-                        user = data.builtBy;
-                    } else if (jsonObject.actions[i].causes[j].hasOwnProperty("userName")) {
-                        data.builtBy = jsonObject.actions[i].causes[j].userName;
-                        user = data.builtBy;
-                    } else if (jsonObject.actions[i].causes[j].hasOwnProperty("upstreamProject")) {
-                        data.builtBy = 'upstream ' + jsonObject.actions[i].causes[j].upstreamProject;
-                    } else if (jsonObject.actions[i].causes[j].hasOwnProperty("addr")) {
-                        data.builtBy = 'remote ' + jsonObject.actions[i].causes[j].addr;
-                    } else {
-                        var myString = jsonObject.actions[i].causes[j].shortDescription;
-                        var myRegexp = /(timer|alarme)/g;
-                        var match = myRegexp.exec(myString);
+  var display_mode = "upstream";
+  if (typeof SURI_DISPLAY_MODE !== 'undefined' && SURI_DISPLAY_MODE !== "") {
+    display_mode = SURI_DISPLAY_MODE;
+  }
 
-                        if (match !== null) {
-                            data.builtBy = match[0];
-                        } else {
-                            data.builtBy = "N/A";
-                        }
-                    }
-                }
+  // Convert String to object
+  var jsonObject = JSON.parse(jsonResponse);
+  var user = "";
+
+  for (var i in jsonObject.actions) {
+    if (jsonObject.actions[i].hasOwnProperty("causes")) {
+      for (var j in jsonObject.actions[i].causes) {
+        if (jsonObject.actions[i].causes[j].hasOwnProperty("shortDescription")) {
+          if (jsonObject.actions[i].causes[j].hasOwnProperty("userId")) {
+            data.builtBy = jsonObject.actions[i].causes[j].userId;
+            user = data.builtBy;
+          } else if (jsonObject.actions[i].causes[j].hasOwnProperty("userName")) {
+            data.builtBy = jsonObject.actions[i].causes[j].userName;
+            user = data.builtBy;
+          } else if (jsonObject.actions[i].causes[j].hasOwnProperty("upstreamProject")) {
+            data.builtBy = 'upstream ' + jsonObject.actions[i].causes[j].upstreamProject;
+          } else if (jsonObject.actions[i].causes[j].hasOwnProperty("addr")) {
+            data.builtBy = 'remote ' + jsonObject.actions[i].causes[j].addr;
+          } else {
+            var myString = jsonObject.actions[i].causes[j].shortDescription;
+            var myRegexp = /(timer|alarme)/g;
+            var match = myRegexp.exec(myString);
+
+            if (match !== null) {
+              data.builtBy = match[0];
+            } else {
+              data.builtBy = "N/A";
             }
+          }
         }
+      }
     }
-    
-    //if display mode is user, use it if it was found in json
-    if (display_mode === "user" && user !== "") {
-        data.builtBy = user;    
-    }
+  }
 
-    if (data.builtBy === null) {
-        data.builtBy = "N/A";
-    }
+  //if display mode is user, use it if it was found in json
+  if (display_mode === "user" && user !== "") {
+    data.builtBy = user;
+  }
 
-    data.builtOn = jsonObject.builtOn;
-    data.result = jsonObject.result;
-    data.success = jsonObject.result === "SUCCESS";
-    data.failure = jsonObject.result === "FAILURE";
-    data.unstable = jsonObject.result === "UNSTABLE";
+  if (data.builtBy === null) {
+    data.builtBy = "N/A";
+  }
 
-    data.durationSeconds = Math.round(jsonObject.duration / 1000);
+  data.builtOn = jsonObject.builtOn;
+  data.result = jsonObject.result;
+  data.success = jsonObject.result === "SUCCESS";
+  data.failure = jsonObject.result === "FAILURE";
+  data.unstable = jsonObject.result === "UNSTABLE";
 
-    if (data.durationSeconds > 200) {
-        data.durationMinutes = Math.round(data.durationSeconds / 60);
-    }
+  data.durationSeconds = Math.round(jsonObject.duration / 1000);
 
-    data.building = jsonObject.building === true;
+  if (data.durationSeconds > 200) {
+    data.durationMinutes = Math.round(data.durationSeconds / 60);
+  }
 
-    var date = new Date(jsonObject.timestamp);
-    var day = date.getDate();
-    var month = date.getMonth() + 1;
-    var year = date.getFullYear();
+  data.building = jsonObject.building === true;
 
-    // Hours part from the timestamp
-    var hours = date.getHours();
-    // Minutes part from the timestamp
-    var minutes = "0" + date.getMinutes();
-    // Seconds part from the timestamp
-    var seconds = "0" + date.getSeconds();
+  var date = new Date(jsonObject.timestamp);
+  var day = date.getDate();
+  var month = date.getMonth() + 1;
+  var year = date.getFullYear();
 
-    // Will display time in 10:30:23 format
-    var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
-    var formattedDate = day + '/' + month + '/' + year;
+  // Hours part from the timestamp
+  var hours = date.getHours();
+  // Minutes part from the timestamp
+  var minutes = "0" + date.getMinutes();
+  // Seconds part from the timestamp
+  var seconds = "0" + date.getSeconds();
 
-    data.formattedTime = formattedTime;
-    data.formattedDate = formattedDate;
+  // Will display time in 10:30:23 format
+  var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+  var formattedDate = day + '/' + month + '/' + year;
 
-    return JSON.stringify(data);
+  data.formattedTime = formattedTime;
+  data.formattedDate = formattedDate;
+
+  return JSON.stringify(data);
 }
-
-
--- content/jenkins/widgets/slaveUpDown/script.js
+++ content/jenkins/widgets/slaveUpDown/script.js
@@ -1,65 +1,65 @@
 /*
- * Copyright 2012-2018 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+  * Copyright 2012-2018 the original author or authors.
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  *      http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
 
-function run (){
-    var data = {};
-    var jsonResponse = Packages.call(WIDGET_CONFIG_JENKINS_URL + "/computer/api/json", null, null, null);
-    if (jsonResponse == null) {
-        return null;
-    }
-    // Convert String to object
-    var jsonObject = JSON.parse(jsonResponse);
-    // Create regex
-    var reg = null;
-    if (SURI_REGEX != null){
-        reg = "^"+SURI_REGEX+"$";
-    } else {
-        reg = ".*";
-    }
+function run() {
+  var data = {};
+  var jsonResponse = Packages.call(WIDGET_CONFIG_JENKINS_URL + "/computer/api/json", null, null, null);
+  if (jsonResponse == null) {
+    return null;
+  }
+  // Convert String to object
+  var jsonObject = JSON.parse(jsonResponse);
+  // Create regex
+  var reg = null;
+  if (SURI_REGEX != null) {
+    reg = "^" + SURI_REGEX + "$";
+  } else {
+    reg = ".*";
+  }
 
-    data.nbUp = 0;
-    data.nbDown = 0;
-    data.total = 0;
-    data.slave = [];
+  data.nbUp = 0;
+  data.nbDown = 0;
+  data.total = 0;
+  data.slave = [];
 
-    for (var i in jsonObject.computer){
-        if (jsonObject.computer[i].displayName.match(reg)){
-            if (jsonObject.computer[i].offline || jsonObject.computer[i].temporarilyOffline){
-                var slave = {};
-                slave.name = jsonObject.computer[i].displayName;
-                if (jsonObject.computer[i].offlineCauseReason !== undefined) {
-                    var cause = jsonObject.computer[i].offlineCauseReason.replace(/<(?:.|\n)*?>/gm, '');
-                    slave.offlineCause = cause.split('\n')[0];
-                }
-                data.nbDown ++;
-                data.slave.push(slave);
-            } else {
-                data.nbUp ++;
-            }
+  for (var i in jsonObject.computer) {
+    if (jsonObject.computer[i].displayName.match(reg)) {
+      if (jsonObject.computer[i].offline || jsonObject.computer[i].temporarilyOffline) {
+        var slave = {};
+        slave.name = jsonObject.computer[i].displayName;
+        if (jsonObject.computer[i].offlineCauseReason !== undefined) {
+          var cause = jsonObject.computer[i].offlineCauseReason.replace(/<(?:.|\n)*?>/gm, '');
+          slave.offlineCause = cause.split('\n')[0];
         }
+        data.nbDown++;
+        data.slave.push(slave);
+      } else {
+        data.nbUp++;
+      }
     }
+  }
 
-    if (data.nbUp === 0){
-        Packages.throwFatalError("Error no slave found for Regex :"+reg);
-    }
+  if (data.nbUp === 0) {
+    Packages.throwFatalError("Error no slave found for Regex :" + reg);
+  }
 
-    data.total = data.nbDown + data.nbUp;
-    data.ok = data.total === data.nbUp;
-    data.minimal = data.nbDown > 5;
+  data.total = data.nbDown + data.nbUp;
+  data.ok = data.total === data.nbUp;
+  data.minimal = data.nbDown > 5;
 
 
-    return JSON.stringify(data);
+  return JSON.stringify(data);
 }
