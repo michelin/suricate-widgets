a998ad730ff5032d9fbba4dc86573763e6b86269
Loïc Greffier <loic.greffier_ext@michelin.com>
2021-02-12 18:00:01 +0100

    :art: - Update widget github count issues

---
+++ content/github/widgets/count-issues/script.js
@@ -0,0 +1,56 @@
+/*
+  * Copyright 2012-2018 the original author or authors.
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  *      http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
+
+function run() {
+	var data = {};
+	var issues = [];
+	var page = 1;
+	
+	var response = JSON.parse(
+					Packages.get("https://api.github.com/repos/" + SURI_GITHUB_ORG + "/" + SURI_GITHUB_PROJECT + "/issues?page=" + page + "&per_page=100&state=" + SURI_ISSUES_STATE,
+					"Authorization", "token " + WIDGET_CONFIG_GITHUB_TOKEN));
+	
+	issues = issues.concat(response);
+
+	while (response && response.length > 0) {
+		page++;
+		
+		response = JSON.parse(
+					Packages.get("https://api.github.com/repos/" + SURI_GITHUB_ORG + "/" + SURI_GITHUB_PROJECT + "/issues?page=" + page + "&per_page=100&state=" + SURI_ISSUES_STATE,
+					"Authorization", "token " + WIDGET_CONFIG_GITHUB_TOKEN));
+		
+		issues = issues.concat(response);
+	}
+	
+	issues = issues.filter(function(issue) {
+		if (!issue.pull_request) {
+			return issue;
+		}
+	});
+	
+	data.numberOfIssues = issues.length;
+	
+	if (SURI_PREVIOUS && JSON.parse(SURI_PREVIOUS).numberOfIssues) {
+		data.evolution = ((data.numberOfIssues - JSON.parse(SURI_PREVIOUS).numberOfIssues) * 100 / JSON.parse(SURI_PREVIOUS).numberOfIssues).toFixed(1);
+		data.arrow = data.evolution == 0 ? '' : (data.evolution > 0 ? "up" : "down");
+	}
+	
+	if (SURI_ISSUES_STATE != 'all') {
+		data.issuesState = SURI_ISSUES_STATE;
+	}
+	
+	return JSON.stringify(data);
+}
