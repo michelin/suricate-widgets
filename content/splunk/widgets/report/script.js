ca27510e49fea7ad3fcfa0c2e7f62fc50653828d
Loïc Greffier <loic.greffier_ext@michelin.com>
2021-04-26 13:44:10 +0200

    Update Splunk widget

--- content/splunk/widgets/report/script.js
+++ content/splunk/widgets/report/script.js
@@ -1,3 +1,4 @@
+// Colors for the matrix
 var yellow = "1 0 0 0 0 0 1 0 0 0 0 0 0.25 0 0 0 0 0 1 0";
 var sand = "1 0 0 0 0 0 1 0 0 0 -0.2 0.2 0.1 0.4 0 0 0 0 1 0";
 var green = "-1 0.035 0.035 0.035 1 0 -1 0.11 0.11 1 0 0 -1 0 1 0 0 0 1 0";
@@ -7,16 +8,16 @@ var emerald = "-1 0 0 0 1.076 0 -1 0.39 0.095 1.1 0 0 -1 0.43 1.1 0 0 0 1 0";
 var white = "1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 1 0";
 var softGrey = "1 0 0 0 -0.225 0 1 0 0 -0.24 0 0 1 0 -0.255 0 0 0 1 0";
 var darkGrey = "-1 0.116 0 0 1 0 -1 0.165 0 1 0 0 -1 0.227 1 0 0 0 1 0";
-
+var splunk = "0.45 0 0 0 0 0 0.69 0 0 0 0 0 0.22 0 0 0 0 0 1 0";
 
 function run() {
 	var data = JSON.parse(SURI_PREVIOUS);
 
 	data.textColor = "#FFFFFF";
 
-	if (typeof SURI_COLOR === 'undefined' || SURI_COLOR === null || SURI_COLOR === '' || SURI_COLOR.contains("CHOICE6")) { //White
-		data.hexaColor = "#FFFFFF";
-		data.matrixColor = white;
+	if (typeof SURI_COLOR === 'undefined' || SURI_COLOR === null || SURI_COLOR === '' || SURI_COLOR.contains("CHOICE0")) {
+		data.hexaColor = "#73AF37";
+		data.matrixColor = splunk;
 		data.textColor = "#000000";
 	} else if (SURI_COLOR.contains("CHOICE1")) {
 		data.hexaColor = "#FFFF89";
@@ -34,6 +35,10 @@ function run() {
 	} else if (SURI_COLOR.contains("CHOICE5")) {
 		data.hexaColor = "#FFFFBB";
 		data.matrixColor = sand;
+		data.textColor = "#000000";
+	} else if (SURI_COLOR.contains("CHOICE6")) {
+		data.hexaColor = "#FFFFFF";
+		data.matrixColor = white;
 		data.textColor = "#000000";
 	} else if (SURI_COLOR.contains("CHOICE7")) {
 		data.hexaColor = "#5C8100";
