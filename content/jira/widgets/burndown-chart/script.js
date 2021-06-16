18a9d99e17d356a9ad4fd25ea1eb7d6032bc7f4a
Loïc Greffier <loic.greffier_ext@michelin.com>
2021-06-16 11:54:09 +0200

    Update the burndown widget

--- content/jira/widgets/burndown-chart/script.js
+++ content/jira/widgets/burndown-chart/script.js
@@ -20,29 +20,27 @@ function run() {
 	data.data = [];
 	data.labels = [];
 
-	// customfield_10611 -> Greenhopper field for sprint data
-	// customfield_10382 -> Story point
-	var jsonResponse = Packages.get(WIDGET_CONFIG_JIRA_URL + "/jra/rest/api/2/search?jql=" + encodeURIComponent(SURI_JQL) + "&fields=customfield_10382,resolutiondate,fixVersions,updated,customfield_10611", "Authorization", "Basic " + Packages.btoa(WIDGET_CONFIG_JIRA_USER + ":" + WIDGET_CONFIG_JIRA_PASSWORD));
+	// customfield_10611 is the Green Hopper field for sprint data
+	// customfield_10382 contains the story points
+	var jsonResponse = Packages.get(WIDGET_CONFIG_JIRA_URL + "/jra/rest/api/2/search?jql=" 
+						+ encodeURIComponent(SURI_JQL) 
+						+ "&fields=customfield_10382,resolutiondate,fixVersions,updated,customfield_10611&maxResults=1000", 
+						"Authorization", "Basic " + Packages.btoa(WIDGET_CONFIG_JIRA_USER + ":" + WIDGET_CONFIG_JIRA_PASSWORD));
+						
 	if (jsonResponse == null) {
 		return null;
 	}
 
-	var jsonObject = JSON.parse(jsonResponse);
-
-	jsonObject.issues.forEach(function(issue) {
-		if (issue.fields.customfield_10611) {
-			updateSprintDate(data, issue.fields.customfield_10611[0]);
-		} else {
-			if (!data.releaseDate && issue.fields.fixVersions[0]) {
-				var release = new Date(issue.fields.fixVersions[0].releaseDate);
-				release.setUTCHours(0, 0, 0, 0);
-				data.releaseDate = release.getTime();
-			}
-		  
-			updateStartDate(data, issue.fields.resolutiondate);
-			updateStartDate(data, issue.fields.updated);
-		}
-
+	var issues = JSON.parse(jsonResponse).issues;
+	
+	// Compute the sprint date from the custom field customfield_10611
+	computeSprintDatesFromCustomField10611(data, issues);
+	
+	if (!data.startDate || !data.endDate) {
+		computeSprintDatesFromOtherFields(data, issues);
+	}
+	
+	issues.forEach(function(issue) {
 		if (issue.fields.customfield_10382) {
 			data.total += issue.fields.customfield_10382;
 		} else {
@@ -50,14 +48,14 @@ function run() {
 		}
 	});
 
-	for (loopTime = data.startDate; loopTime < data.releaseDate; loopTime += 86400000) {
+	for (loopTime = data.startDate; loopTime < data.endDate; loopTime += 86400000) {
 		data.labels.push(loopTime);
 		data.data.push(data.total);
 	}
 
 	// For all issues, check the resolution date. If set, decrease the number of issues to solve in the chronology starting from the
 	// resolution date of the current issue
-	jsonObject.issues.forEach(function(issue) {
+	issues.forEach(function(issue) {
 		if (issue.fields.resolutiondate) {
 		  var dec = 1
 		  if (issue.fields.customfield_10382 && issue.fields.customfield_10382 !== 0) {
@@ -85,28 +83,63 @@ function run() {
 	return JSON.stringify(data);
 }
 
-function updateSprintDate(data, fieldData) {
-	if (fieldData.match("startDate=([0-9-]+)")) {
-		var startdate = new Date(fieldData.match("startDate=([0-9-]+)")[1]);
-		startdate.setUTCHours(0, 0, 0, 0);
+/**
+Compute the start date and the end date of the sprint from the field 10611
+**/
+function computeSprintDatesFromCustomField10611(data, issues) {
+	var i = 0;
 		
-		data.startDate = startdate.getTime()
-	}
-	
-	if (fieldData.match("endDate=([0-9-]+)")) {
-		var releaseDate = new Date(fieldData.match("endDate=([0-9-]+)")[1]);
-		releaseDate.setUTCHours(0, 0, 0, 0);
+	// Try to compute the start and end date from the customfield_10611 field
+	while ((!data.startDate || !data.endDate) && i < issues.length) {		
+		if (issues[i].fields.customfield_10611 && issues[i].fields.customfield_10611.length === 1) {
+			var sprintData = issues[i].fields.customfield_10611[0];
+			
+			if (sprintData.match("startDate=([0-9-]+)")) {
+				var startDate = new Date(sprintData.match("startDate=([0-9-]+)")[1]);
+				startDate.setUTCHours(0, 0, 0, 0);
 		
-		data.releaseDate = releaseDate.getTime()
+				data.startDate = startDate.getTime()
+			}
+			
+			if (sprintData.match("endDate=([0-9-]+)")) {
+				var endDate = new Date(sprintData.match("endDate=([0-9-]+)")[1]);
+				endDate.setUTCHours(23, 59, 0, 0);
+		
+				data.endDate = endDate.getTime()
+			}
+		}
+				
+		i++;
 	}
 }
 
+/**
+Compute the start date and the end date of the sprint from the resolutionupdate and updated fields
+**/
+function computeSprintDatesFromOtherFields(data, issues) {
+	issues.forEach(function(issue) {
+		if (issue.fields.fixVersions[0]) {
+			var endDate = new Date(issue.fields.fixVersions[0].releaseDate);
+			endDate.setUTCHours(0, 0, 0, 0);
+			
+			data.endDate = endDate.getTime();
+		}
+		
+		updateStartDate(data, issue.fields.resolutiondate);
+		updateStartDate(data, issue.fields.updated);
+	});
+}
+
+/**
+Update the current start date from the given date
+**/
 function updateStartDate(data, date) {
-  if (date) {
-    var startdate = new Date(date);
-    startdate.setUTCHours(0, 0, 0, 0);
-    if (data.startDate === undefined || data.startDate > startdate.getTime()) {
-      data.startDate = startdate.getTime()
-    }
-  }
+	if (date) {
+		var startDate = new Date(date);
+		startDate.setUTCHours(0, 0, 0, 0);
+    
+		if (data.startDate === undefined || data.startDate > startDate.getTime()) {
+			data.startDate = startDate.getTime();
+		}
+	}
 }
