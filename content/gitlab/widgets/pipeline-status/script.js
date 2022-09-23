ca30907ef1bf4d387b1fb9c9dde7efdf86cabde1
Tolrod22 <loic.travaille@gmail.com>
2022-09-23 17:06:24 +0200

    fix: gitlab pipeline status

--- content/gitlab/widgets/pipeline-status/script.js
+++ content/gitlab/widgets/pipeline-status/script.js
@@ -20,49 +20,70 @@ function run() {
 	data.project = JSON.parse(
 		Packages.get(WIDGET_CONFIG_GITLAB_URL + "/api/v4/projects/" + SURI_PROJECT, "PRIVATE-TOKEN", WIDGET_CONFIG_GITLAB_TOKEN)).name;
 
-	var url = WIDGET_CONFIG_GITLAB_URL + "/api/v4/projects/" + SURI_PROJECT + "/pipelines?per_page=100&page=1";
-	
+	var url = WIDGET_CONFIG_GITLAB_URL + "/api/v4/projects/" + SURI_PROJECT + "/pipelines";
+
 	if (SURI_PROJECT_BRANCH) {
-		url += "&ref=" + SURI_PROJECT_BRANCH;
+		url += "?ref=" + SURI_PROJECT_BRANCH;
 	}
-	
+
 	var pipelines = JSON.parse(Packages.get(url, "PRIVATE-TOKEN", WIDGET_CONFIG_GITLAB_TOKEN));
-	
+
 	if (pipelines && pipelines.length > 0) {
-		data.status = pipelines[0].status;
-		
 		var pipeline = JSON.parse(
-			Packages.get(WIDGET_CONFIG_GITLAB_URL + "/api/v4/projects/" + SURI_PROJECT + "/pipelines/" + pipelines[0].id + "?per_page=100&page=1", "PRIVATE-TOKEN", WIDGET_CONFIG_GITLAB_TOKEN));
-	
+			Packages.get(WIDGET_CONFIG_GITLAB_URL + "/api/v4/projects/" + SURI_PROJECT + "/pipelines/" + pipelines[0].id, "PRIVATE-TOKEN", WIDGET_CONFIG_GITLAB_TOKEN));
+
+		data.status = pipeline.detailed_status.group;
 		data.name = pipeline.user.name;
 		data.username = pipeline.user.username;
 		data.branch = pipeline.ref;
 		data.url = pipeline.web_url;
 		data.createdAt = formatDate(pipeline.created_at);
-		
-		if (pipeline.duration) {
-            data.duration = secondsToDuration(pipeline.duration);
-        } else {
-			data.duration = 'N/A';
-		}
-		
-		if (pipeline.started_at) {
-			data.waitingTimeBeforeStarting = secondsToDuration((new Date(pipeline.started_at).getTime() - new Date(pipeline.created_at).getTime()) / 1000);
+
+		if (HIDE_TIMETABLE && HIDE_TIMETABLE === 'true') {
+			data.hideTimeTable = HIDE_TIMETABLE;
 		} else {
-			data.waitingTimeBeforeStarting = secondsToDuration((new Date().getTime() - new Date(pipeline.created_at).getTime()) / 1000) + (' (not started yet)');
+			if (pipeline.duration) {
+				data.duration = secondsToDuration(pipeline.duration);
+			} else {
+				data.duration = 'N/A';
+			}
+			if (pipeline.started_at) {
+				data.waitingTimeBeforeStarting = secondsToDuration((new Date(pipeline.started_at).getTime() - new Date(pipeline.created_at).getTime()) / 1000);
+			} else {
+				data.waitingTimeBeforeStarting = secondsToDuration((new Date().getTime() - new Date(pipeline.created_at).getTime()) / 1000) + (' (not started yet)');
+			}
 		}
-		
+
+		if (SHOW_FAILED_JOBS && SHOW_FAILED_JOBS === 'true') {
+			data.showFailedJobs = SHOW_FAILED_JOBS;
+			if (data.status === 'success-with-warnings' || data.status === 'failed') {
+				var failed_jobs = JSON.parse(
+					Packages.get(WIDGET_CONFIG_GITLAB_URL + "/api/v4/projects/" + SURI_PROJECT + "/pipelines/" + pipeline.id + "/jobs?scope=failed&per_page=100&page=1", "PRIVATE-TOKEN", WIDGET_CONFIG_GITLAB_TOKEN));
+					data.failed_jobs = "";
+				for (var i = 0; i < failed_jobs.length; i++) {
+					data.failed_jobs = data.failed_jobs + failed_jobs[i].name + ", ";
+				}
+				data.failed_jobs = data.failed_jobs.substring(0, data.failed_jobs.length - 2);
+			}
+		}
+
 		if (data.status === 'success') {
 			data.success = true;
+		} else if (data.status === 'success-with-warnings') {
+			data.warning = true;
 		} else if (data.status === 'failed') {
 			data.failed = true;
 		} else if (data.status === 'running') {
 			data.running = true;
+		} else if (data.status === 'canceled') {
+			data.canceled = true;
+		} else if (data.status === 'skipped') {
+			data.skipped = true;
 		} else {
 			data.otherStatus = true;
 		}
 	}
-		
+
 	return JSON.stringify(data);
 }
 
@@ -82,11 +103,11 @@ function formatDate(date) {
     var hours = new Date(date).getHours() < 10 ? "0" + new Date(date).getHours() : new Date(date).getHours();
     var minutes = new Date(date).getMinutes() < 10 ? "0" + new Date(date).getMinutes() : new Date(date).getMinutes();
     var seconds = new Date(date).getSeconds() < 10 ? "0" + new Date(date).getSeconds() : new Date(date).getSeconds();
-	
-	return new Date(date).getFullYear() 
-			+ "-" 
-			+ ("0" + (new Date(date).getMonth() + 1)).slice(-2) 
-			+ "-" 
+
+	return new Date(date).getFullYear()
+			+ "-"
+			+ ("0" + (new Date(date).getMonth() + 1)).slice(-2)
+			+ "-"
 			+ ("0" + new Date(date).getUTCDate()).slice(-2)
 			+ " at "
 			+ hours
