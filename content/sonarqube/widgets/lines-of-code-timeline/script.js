788ae11d8c4d805a8ae6c22f65677ab05bc17dcb
Loïc GREFFIER <loic.greffier@michelin.com>
2025-01-08 19:24:14 +0100

    Fix Sonar coverage widget (#49)
    * Add Sonar coverage and fix sonar metrics rendering
    
    * Fix Sonar coverage widget

---
+++ content/sonarqube/widgets/lines-of-code-timeline/script.js
@@ -0,0 +1,240 @@
+/*
+  * Copyright 2012-2021 the original author or authors.
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  *      http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
+var data = {};
+
+function run() {
+	var projectPage = 1;
+	var pageSize = 20;
+	var sonarUrl = WIDGET_CONFIG_SONAR_URL.replace(/\/+$/, "");
+    var sonarToken = WIDGET_CONFIG_SONAR_TOKEN;
+    var monthNames = [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec",
+      ];
+
+	var locMetrics = [];
+	data.datasets = [];
+	data.projectNames = '';
+
+	data.fromDate = computeStartDate();
+	var projectNames = SURI_PROJECT.split(",");
+	var labels = [];
+	var locPerDateArr = [];
+	var locData = [];
+    var datasets = [];
+
+	projectNames.forEach(function(projectKey, index) {
+
+	    locMetrics = [];
+	    locPerDateArr = [];
+        data.projectNames += projectKey.split(":")[1] + ", ";
+		projectPage = 1;
+
+		var response = getLinesOfCodeFromSonarQube(sonarUrl,sonarToken,projectKey,pageSize,projectPage,data.fromDate);
+		if (response && response.measures[0] && response.measures[0].history && response.measures[0].history.length > 0) {
+		    locMetrics = locMetrics.concat(response.measures[0].history);
+	     }
+		while (response.paging.total >
+                     response.paging.pageIndex * response.paging.pageSize) {
+			projectPage++;
+			var response = getLinesOfCodeFromSonarQube(sonarUrl,sonarToken,projectKey,pageSize,projectPage,data.fromDate);
+			locMetrics = locMetrics.concat(response.measures[0].history);
+		}
+
+		locMetrics.forEach(function (locHistory) {
+            dateExtract = new Date(locHistory.date);
+            dateFirstTen =
+              dateExtract.getFullYear() +
+              "-" +
+              ("0" + (dateExtract.getMonth() + 1)).slice(-2) +
+              "-" +
+              ("0" + dateExtract.getUTCDate()).slice(-2);
+
+            if (SURI_PERIOD == "Day") {
+              displayDate = dateFirstTen;
+            } else if (SURI_PERIOD == "Week") {
+              displayDate = dateFirstTen;
+            } else if (SURI_PERIOD == "Month") {
+              displayDate =
+                monthNames[dateExtract.getMonth()] + "," + dateExtract.getFullYear();
+            } else if (SURI_PERIOD == "Year") {
+              displayDate = dateExtract.getFullYear().toString();
+            }
+
+            if (labels.indexOf(displayDate) == -1) {
+                labels.push(displayDate.toString());
+            }
+            locPerDateArr.push(displayDate+":"+locHistory.value);
+        });
+
+        labels.sort(orderMetricsByDate);
+
+        locData = [];
+        labels.forEach(function(displayDate) {
+          for (var i = locPerDateArr.length - 1; i >= 0; i--) {
+            var locPerDate = locPerDateArr[i];
+            var entryDate = locPerDate.split(":")[0];
+
+            if(entryDate === displayDate) {
+              locData.push({
+
+                  x: displayDate,
+                  y: locPerDate.split(":")[1]
+              });
+              break;
+            }
+          }
+
+        });
+
+        datasets.push(JSON.stringify({
+            label : projectKey.split(":")[1] != null ? projectKey.split(":")[1] : projectKey.split(":")[0],
+            data : locData,
+            backgroundColor: getColorForTrigram(index, '0.2'),
+            borderColor: getColorForTrigram(index, '0.6'),
+            pointBackgroundColor: getColorForTrigram(index, '1'),
+            pointBorderColor: getColorForTrigram(index, '0.6'),
+            pointBorderWidth: 5
+
+        })
+        );
+    });
+    data.labels = JSON.stringify(labels);
+    data.datasets = JSON.stringify(datasets.every(zeroTest) ? [] : datasets);
+    return JSON.stringify(data);
+}
+
+function zeroTest(element) {
+    return element === 0;
+}
+
+/**
+ * Compute the start date of the loc metrics from the widget parameters
+ * @returns {string}
+ */
+function computeStartDate() {
+    var computedDate = new Date();
+	if (SURI_PERIOD) {
+		var numberOfPeriods = 1;
+		if (SURI_NUMBER_OF_PERIOD) {
+			numberOfPeriods = SURI_NUMBER_OF_PERIOD;
+		}
+
+		 if (SURI_PERIOD === "Day") {
+              computedDate.setDate(new Date().getDate() - numberOfPeriods);
+         } else if (SURI_PERIOD === "Week") {
+			computedDate.setDate(new Date().getDate() - 7 * numberOfPeriods);
+		} else if (SURI_PERIOD === "Month") {
+			computedDate.setMonth(new Date().getMonth() - numberOfPeriods);
+		} else if (SURI_PERIOD === "Year") {
+			computedDate.setFullYear(new Date().getFullYear() - numberOfPeriods);
+		}
+
+		computedDate.setUTCHours(0, 0, 0, 0);
+	}
+	return formatDate(computedDate);
+}
+
+/**
+ * Format the date to keep yyyy-MM-dd
+ */
+function formatDate(date) {
+	return new Date(date).getFullYear()
+		+ "-"
+		+ ("0" + (new Date(date).getMonth() + 1)).slice(-2)
+		+ "-"
+		+ ("0" + new Date(date).getUTCDate()).slice(-2);
+}
+
+function getLinesOfCodeFromSonarQube(
+  sonarUrl,
+  sonarToken,
+  projectKeys,
+  pageSize,
+  projectPage,
+  fromDate
+) {
+  var sonarApiUrl =
+    sonarUrl +
+    "/api/measures/search_history?" +
+    (SURI_BRANCH != null ? "branch=" + SURI_BRANCH + "&" : (SURI_PULL_REQUEST != null ? "pullRequest=" + SURI_PULL_REQUEST + "&" : "")) +
+    "metrics=ncloc&component=" +
+    projectKeys +
+    "&ps=" +
+    pageSize +
+    "&p=" +
+    projectPage;
+
+  if (fromDate) {
+    sonarApiUrl = sonarApiUrl + "&from=" + fromDate;
+  }
+
+  return JSON.parse(
+    Packages.get(
+      sonarApiUrl,
+      "Authorization",
+      "Basic " + Packages.btoa(sonarToken + ":")
+    )
+  );
+}
+
+
+/**
+ * Keep unique value in array
+ *
+ * @param value The value
+ * @param index The index
+ * @param self The array
+ * @returns {boolean}
+ */
+function onlyUnique(value, index, self) {
+	return self.indexOf(value) === index;
+}
+
+/**
+ * Generate RGBA color for data
+ *
+ * @param index The index of the entity to color
+ * @param opacity The opacity
+ * @returns {string}
+ */
+function getColorForTrigram(index, opacity) {
+	return 'rgba(' + ((255 - index * 12) % 256) + ',' + ((102 + index * 12) % 256) + ',' + ((255 + index * 12) % 256) + ',' + opacity + ')';
+}
+
+function orderMetricsByDate(firstCommit, secondCommit) {
+	if (new Date(firstCommit) < new Date(secondCommit)){
+		return -1;
+	}
+
+	if (new Date(firstCommit) > new Date(secondCommit)){
+		return 1;
+	}
+	return 0;
+}
+
+
+
---
+++ content/sonarqube/widgets/project-coverage/script.js
@@ -0,0 +1,50 @@
+/*
+  * Copyright 2012-2021 the original author or authors.
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  *      http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
+
+function run() {
+	var data = {};
+	var branch = (SURI_BRANCH != null ? "branch=" + SURI_BRANCH + "&" : (SURI_PULL_REQUEST != null ? "pullRequest=" + SURI_PULL_REQUEST + "&" : ""));
+	data.results = [];
+
+	// Added to remove the trailing slash from the URL if present
+	data.sonarConfigUrl = (WIDGET_CONFIG_SONAR_URL) ? WIDGET_CONFIG_SONAR_URL.replace(/\/+$/, '') : WIDGET_CONFIG_SONAR_URL;
+
+	var response = JSON.parse(Packages.get(data.sonarConfigUrl 
+		+ "/api/measures/component?" + branch
+		+ "component=" 
+		+ SURI_PROJECT_KEY 
+		+ "&additionalFields=metrics&metricKeys=" 
+		+ SURI_METRICS,
+    	"Authorization", "Basic " + Packages.btoa(WIDGET_CONFIG_SONAR_TOKEN + ":")));
+	
+	print(JSON.stringify(response));
+	if (response && response.component && response.component.measures && response.component.measures.length > 0) {
+		response.component.measures.forEach(function(measure) {
+			data.results.push({
+				title: response.metrics.filter(function(metric) {
+							if (metric.key == measure.metric) {
+								return metric;
+							} 
+						})[0].name,
+				value: measure.periods[0].value,
+
+			});
+		});
+
+	}
+
+  return JSON.stringify(data);
+}
